77a35de64181aa3f4520cd6568094034862a4232
exxxecutor <rudezwloki7@gmail.com>
2019-11-08 10:49:54 +0300

    I AM STRONG

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        int N =scan.nextInt();
+        for(int i=0; i<N;i++) {
+            System.out.print("MUD GOLEM STUNNED UR ASS FOR "+N*0.3+" SECONDS");
+        }
+    }
+}
