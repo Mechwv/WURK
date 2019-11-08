cf2746ba35893d43256f76f7f2961c427d2072c5
R1k0523 <isergey555345@gmail.com>
2019-11-08 10:55:39 +0300

    I AM STRONGER THEN YOU

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,7 @@ public class Main {
         for(int i=0; i<N;i++) {
             System.out.print("MUD GOLEM STUNNED UR ASS FOR "+N*0.3+" SECONDS");
         }
+        System.out.print("GOLEM CRIES 'HOES MAD' ");
+        System.out.print("GOLEM iINFLICTED YOU 9000+ DAMAGE");
     }
 }
