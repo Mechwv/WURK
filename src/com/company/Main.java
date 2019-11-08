10cdad807bab1eff3ceb24a02bbbea528a7be232
exxxecutor <rudezwloki7@gmail.com>
2019-11-08 11:58:03 +0300

    GAYME

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,38 @@
 package com.company;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
 import java.util.Scanner;
+
+import static com.sun.java.accessibility.util.AWTEventMonitor.addKeyListener;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner (System.in);
-        int N =scan.nextInt();
-        for(int i=0; i<N;i++) {
-            System.out.print("MUD GOLEM STUNNED UR ASS FOR "+N*0.3+" SECONDS");
+        int s = 0;
+        int p = 0;
+        char ch ;
+        int code = scan.next().charAt(0);
+        while (code  != 'q')
+        {
+            code = scan.next().charAt(0);
+            ch = (char) code ;
+            // выйти когда нажато 'q'
+            if ( 's' == ch )
+            {
+                System.out.println("MUD GOLEM STUNNED UR ASS FOR " + (((s * 0.3) * 10) / 10) + " SECONDS");
+                s++;
+            }
+            if ( 'p' == ch)
+            {
+                System.out.println("MUD GOLEM PUNCHED UR ASS FOR " + (((p * 30) * 10) / 10) + " DAMAGE");
+                p++;
+            }
         }
-        System.out.print("GOLEM CRIES 'HOES MAD' ");
-        System.out.print("GOLEM iINFLICTED YOU 9000+ DAMAGE");
-        for(int i=0; i<N;i++) {
-            System.out.print("MUD GOLEM PUNCHED UR ASS FOR "+N*30+" DAMAGE");
-        }
-        System.out.print("GOLEM CRIES ON RUSSIAN 'МАГМА ТЕЧЕТ В НАШИХ ВЕНАХ, РАСКАЛЯЯ СЕРДЦА! БЕЗГРАНИЧНОЕ СЛИЯНИЕ! ГУРРРРРРЕН ЛАГГАААААААН!!!!!!!! ВЕДЬ Я. ВЕДЬ МЫ. ДА ЗА КОГО ВЫ НАС ДЕРЖИТЕ' ");
+
+        System.out.println("GOLEM CRIES 'HOES MAD' ");
+        System.out.println("GOLEM iINFLICTED YOU 9000+ DAMAGE");
+        System.out.println("GOLEM CRIES ON RUSSIAN 'МАГМА ТЕЧЕТ В НАШИХ ВЕНАХ, РАСКАЛЯЯ СЕРДЦА! БЕЗГРАНИЧНОЕ СЛИЯНИЕ! ГУРРРРРРЕН ЛАГГАААААААН!!!!!!!! ВЕДЬ Я. ВЕДЬ МЫ. ДА ЗА КОГО ВЫ НАС ДЕРЖИТЕ' ");
     }
 }
